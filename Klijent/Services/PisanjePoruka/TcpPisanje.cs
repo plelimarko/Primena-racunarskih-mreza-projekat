6badb73f198c785d6eeb33330ad98b93a51b0d03
Pele14 <aleksa.pele03@gmail.com>
2025-08-15 01:43:47 +0200

    pisanje

---
+++ Klijent/Services/PisanjePoruka/TcpPisanje.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+namespace Services.PisanjePoruka
+{
+    public class TcpPisanje
+    {
+        public bool PosaljiPoruku(Socket socket, string poruka)
+        {
+            bool poslato;
+            byte[] binarnaPoruka = Encoding.UTF8.GetBytes(poruka);
+
+            try
+            {
+                socket.Send(binarnaPoruka, 0, binarnaPoruka.Length, SocketFlags.None);
+                poslato = true;
+            }
+            catch (Exception)
+            {
+                poslato= false;
+            }
+
+            return poslato;
+        }
+    }
+}
---
+++ Klijent/Services/PisanjePoruka/UdpPisanje.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.PisanjePoruka
+{
+    internal class UdpPisanje
+    {
+        public bool PosaljiPoruku(Socket socket, string poruka, EndPoint primaocEP)
+        {
+            bool poslato;
+            byte[] binarnaPoruka = Encoding.UTF8.GetBytes(poruka);
+
+            try
+            {
+                socket.SendTo(binarnaPoruka, 0, binarnaPoruka.Length, SocketFlags.None, primaocEP);
+                poslato = true;
+            }
+            catch (Exception e)
+            {
+                poslato = false;
+                Console.WriteLine($"Doslo je do greske: {e.Message}");
+            }
+
+            return poslato;
+        }
+    }
+}
