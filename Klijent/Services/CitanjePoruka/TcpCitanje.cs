19a2b8dfe4c67ab6e6bc18dea1789a61c2d9bf03
Pele14 <aleksa.pele03@gmail.com>
2025-08-15 01:38:06 +0200

    citanje poruka

---
+++ Klijent/Services/CitanjePoruka/TcpCitanje.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+namespace Services.CitanjePoruka
+{
+    public class TcpCitanje
+    {
+        public string ProcitajPoruku(Socket socket)
+        {
+            string poruka;
+            byte[] buffer = new byte[1024];
+
+            try
+            {
+                int bytesReceived = socket.Receive(buffer);
+                poruka = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+            }
+            catch (Exception e)
+            {
+                poruka = String.Empty;
+            }
+
+            return poruka;
+        }
+    }
+}
---
+++ Klijent/Services/CitanjePoruka/UdpCitanje.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.CitanjePoruka
+{
+    public class UdpCitanje
+    {
+        public string ProcitajPoruku(Socket socket, ref EndPoint posiljaocEP)
+        {
+            string poruka;
+            byte[] buffer = new byte[1024];
+
+            try
+            {
+                int bytesReceived = socket.ReceiveFrom(buffer, ref posiljaocEP);
+                poruka = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+            }
+            catch (Exception e)
+            {
+                poruka = String.Empty;
+            }
+
+            return poruka;
+        }
+    }
+}
